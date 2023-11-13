3e530e6c950970ac44ae404ccdddc791660ded86
Daniel Martin <danielmartin1444@gmail.com>
2023-11-13 14:07:15 +0000

    Improved readability and cleaned up some duplicate code

--- Personal Projects/CookieClicker Game/res/script.js
+++ Personal Projects/CookieClicker Game/res/script.js
@@ -8,58 +8,67 @@ LoadCookiesFromLocalStorage();
 UpdateCookiesClicked();
 AddClickEvent();
 
-function LoadCookiesFromLocalStorage() {
+function LoadCookiesFromLocalStorage() 
+{
     if(!localStorage.getItem("cookies"))
         return;
-    else 
+    else{
         totalCookies = localStorage.getItem("cookies");
         UpdateCookiesClicked();
+    }
 }
 
-function SaveCookiesToLocalStorage() {
+function SaveCookiesToLocalStorage() 
+{
     localStorage.setItem("cookies", totalCookies);
 }
 
-function UpdateCookiesClicked() {
+function UpdateCookiesClicked() 
+{
     const newText = `Cookies Clicked: ${totalCookies}`;
     document.getElementById("cookiesTotal").textContent = newText;
 }
 
-function RemoveCookies(totalCookiesToRemove) {
+function RemoveCookies(totalCookiesToRemove) 
+{
     totalCookies -= totalCookiesToRemove;
     SaveCookiesToLocalStorage();
     UpdateCookiesClicked();
 }
 
+function AddClickListener(upgradeName, upgrade, requiredCookies, newCookieMultipler)
+{
+    if(!upgrade && totalCookies >= requiredCookies)
+    {
+        upgrade = true;
+        cookieMultiplier = newCookieMultipler;
+        RemoveCookies(requiredCookies);
+        
+        document.getElementById(`${upgradeName}`).removeEventListener('click');
+    }
+}
+
 function AddClickEvent()
 {
-    document.getElementById("cookie").addEventListener('click', function() {
+    document.getElementById("cookie").addEventListener('click', function() 
+    {
         CookieClicked();
         UpdateCookiesClicked();
     }, false);
 
-    document.getElementById("twoCookiesUpgrade").addEventListener('click', function() {
-        if(!twoTimesCookiesActivated && totalCookies >= 200) {
-            twoTimesCookiesActivated = true;
-            cookieMultiplier = 2;
-            RemoveCookies(200);
-            
-            document.getElementById("twoCookiesUpgrade").removeEventListener('click');
-        }
+    document.getElementById("twoCookiesUpgrade").addEventListener('click', function() 
+    {
+        AddClickListener("twoCookiesUpgrade", twoTimesCookiesActivated, 200, 2);
     }, false);
 
-    document.getElementById("fourCookiesUpgrade").addEventListener('click', function() {
-        if(!fourTimesCookiesActivated && totalCookies >= 400) {
-            fourTimesCookiesActivated = true;
-            cookieMultiplier = 4;
-            RemoveCookies(400);
-
-            document.getElementById("fourCookiesUpgrade").removeEventListener('click');
-        }
+    document.getElementById("fourCookiesUpgrade").addEventListener('click', function() 
+    {
+        AddClickListener("fourCookiesUpgrade", fourTimesCookiesActivated, 400, 4);
     }, false);
 }
 
-const CookieClicked = () => {
+const CookieClicked = () => 
+{
     totalCookies += cookieMultiplier;
     SaveCookiesToLocalStorage();
 }
